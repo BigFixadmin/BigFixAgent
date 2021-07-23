89f7a272690604f9175d3a15b7b387b1219e0752
BigFixadmin <87861257+BigFixadmin@users.noreply.github.com>
2021-07-23 22:15:58 +0530

    Update ba1.sh

--- .github/workflows/ba1.sh
+++ .github/workflows/ba1.sh
@@ -3,8 +3,8 @@
 #date :02 04 2021 
 mkdir BESClient
 cd BESClient
-wget https://github.com/BigFixadmin/BigFixAgent/blob/main/BESAgent-10.0.1.41-ubuntu10.amd64.deb
-wget https://github.com/BigFixadmin/BigFixAgent/blob/main/actionsite.afxm
+wget https://raw.githubusercontent.com/BigFixadmin/BigFixAgent/main/BESAgent-10.0.1.41-ubuntu10.amd64.deb
+wget https://raw.githubusercontent.com/BigFixadmin/BigFixAgent/main/actionsite.afxm
   dpkg -i BESAgent-10.0.1.41-ubuntu10.amd64.deb
 
 /etc/init.d/besclient start
